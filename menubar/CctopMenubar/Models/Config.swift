138a25e878e17ea1ffcb1fc2bd6003f2cd626f87
st0012 <stan001212@gmail.com>
2026-07-29 11:13:59 +0100

    Bump version to 0.20.1

--- menubar/CctopMenubar/Models/Config.swift
+++ menubar/CctopMenubar/Models/Config.swift
@@ -16,7 +16,7 @@ private struct CodexConfigFile: Decodable {
 }
 
 enum Config {
-    static let hookVersion = "0.20.0"
+    static let hookVersion = "0.20.1"
     private static let codexStateDatabaseFilename = "state_5.sqlite"
 
     static func sessionsDir() -> String {
